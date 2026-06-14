7fd98ddcccfb12389747a617b74e3ec8f96028f4
aniketkumar19826 <aniketkumar19826@gmail.com>
2026-06-14 15:04:59 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
+    ],
+  },
+
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb",
+    },
+  },
+};
+
+export default nextConfig;
---
+++ postcss.config.mjs
@@ -0,0 +1,32 @@
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+
+// module.exports = {
+//   plugins: {
+//     '@tailwindcss/postcss': {},
+//     autoprefixer: {},
+//   },
+// }
+
+// /** @type {import('postcss-load-config').Config} */
+// const config = {
+//   plugins: {
+//     // tailwindcss: {},
+//     '@tailwindcss/postcss': {},
+//     autoprefixer: {}
+//   },
+// };
+
+// export default config;
+
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+
